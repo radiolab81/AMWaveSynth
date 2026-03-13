0eed0bb3550f98585032a6c974e1fb496c8f13f8
radiolab <48355256+radiolab81@users.noreply.github.com>
2026-03-13 06:55:54 +0000

    Update build_modulators.sh

--- build_modulators.sh
+++ build_modulators.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-gcc -O3 -march=native am_modulator_multiple_channels.c -o am_modulator -lm -lpthread -lliquid
-gcc -O3 -march=native am_modulator_multiple_channels_5MSPS.c -o am_modulator_5MSPS -lm -lpthread -lliquid
+gcc -O3 -march=native am_modulator_multiple_channels.c -o am_modulator -lpthread -lliquid -lm
+gcc -O3 -march=native am_modulator_multiple_channels_5MSPS.c -o am_modulator_5MSPS -lpthread -lliquid -lm
 cp am_modulator amtxgui
 cp am_modulator_5MSPS amtxgui
