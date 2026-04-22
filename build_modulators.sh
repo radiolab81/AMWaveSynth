127b40f3aa858f4d73a14fd2b5a9cc7f79ba07e3
radiolab <48355256+radiolab81@users.noreply.github.com>
2026-04-22 10:24:41 +0000

    Update build_modulators.sh

--- build_modulators.sh
+++ build_modulators.sh
@@ -3,3 +3,4 @@ gcc -O3 -march=native am_modulator_multiple_channels.c -o am_modulator -lpthread
 gcc -O3 -march=native am_modulator_multiple_channels_5MSPS.c -o am_modulator_5MSPS -lpthread -lliquid -lm
 cp am_modulator amtxgui
 cp am_modulator_5MSPS amtxgui
+[ -f amtxgui/start_sender.sh ] && sed -i 's/USE_INT32_MATH_VERSION=true/USE_INT32_MATH_VERSION=false/g' amtxgui/start_sender.sh
